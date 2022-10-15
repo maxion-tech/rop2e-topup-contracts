3265da262bed0e7e8b4fb7dfa6044016db3629f4
Ton Maxion <ton@maxion.tech>
2022-10-15 16:39:16 +0700

    add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,36 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const { CURRENCY_TOKEN_ADDRESS, TREASURY_ADDRESS, PARTNER_ADDRESS, PLATFORM_ADDRESS } = process.env;
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  if (!CURRENCY_TOKEN_ADDRESS ||
+    !TREASURY_ADDRESS ||
+    !PARTNER_ADDRESS ||
+    !PLATFORM_ADDRESS) {
+    throw new Error("Parameter form env file not correct");
+  }
+  const rop2eTopupContractFactory = await ethers.getContractFactory("ROP2ETopupContract");
 
-  await lock.deployed();
+  const treasuryPercent = ethers.BigNumber.from(30 * 10 ** 8); // 30%
+  const partnerPercent = ethers.BigNumber.from(42 * 10 ** 8); // 42%
+  const platformPercent = ethers.BigNumber.from(28 * 10 ** 8); // 28%
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const rop2eTopupContract = await rop2eTopupContractFactory.deploy(
+    CURRENCY_TOKEN_ADDRESS,
+    TREASURY_ADDRESS,
+    PARTNER_ADDRESS,
+    PLATFORM_ADDRESS,
+    treasuryPercent,
+    partnerPercent,
+    platformPercent,
+  );
+
+  await rop2eTopupContract.deployed();
+
+  console.log(`Deployed ROP2E Topup contract to ${rop2eTopupContract.address}`);
+  console.log(`Verify contract by:
+  npx hardhat verify --network ??? ${rop2eTopupContract.address} ${CURRENCY_TOKEN_ADDRESS} ${TREASURY_ADDRESS} ${PARTNER_ADDRESS} ${PLATFORM_ADDRESS} ${treasuryPercent} ${partnerPercent} ${platformPercent}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
